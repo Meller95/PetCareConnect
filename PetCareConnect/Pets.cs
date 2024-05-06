402ceac4343c3c213a16ecd06d3eb2e1060e943c
MaltheSorensen <malthesorensen96@gmail.com>
2024-05-06 10:54:55 +0200

    Tilføjet "Pets.cs" klassen

---
+++ PetCareConnect/Pets.cs
@@ -0,0 +1,22 @@
+﻿namespace PetCareConnect
+{
+    public class Pets
+    {
+        public string Name { get; set; } //Navn på kæledyert
+        public string Species { get; set; } // Art af kældedyr
+        public string Breed { get; set; } // Race af kæledyr
+        public int Age { get; set; } // Alder på kæledyret
+        public string Info { get; set; } // Ekstra information om kæledyret
+        public string OwnerName { get; set; } // Brugernavn på kæledyrets ejer
+
+        public Pets(string name, string species, string breed, int age, string info, string ownername)
+        {
+            Name = name;
+            Species = species;
+            Breed = breed;
+            Age = age;
+            Info = info;
+            OwnerName = ownername;
+        }
+    }
+}
